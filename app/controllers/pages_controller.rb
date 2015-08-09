dd457b747e236589d62e9e4992cf6b4bf8ab94b4
Nathan Anderson <nallenanderson@gmail.com>
2015-08-09 14:41:13 -0300

    Added English

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,5 +3,9 @@ class PagesController < ApplicationController
   def home
   	@lead = Lead.new
   end
+
+  def en
+  	@lead = Lead.new
+  end
   
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
 	resources :leads
 	
   root 'pages#home'
+
+  get 'en' => 'pages#en'
 end
