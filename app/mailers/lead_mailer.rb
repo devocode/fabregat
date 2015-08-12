f5913c781f68d25889924b6b11d02933282c418b
Nathan Anderson <nallenanderson@gmail.com>
2015-08-12 16:55:51 -0300

    really rocking...

--- app/mailers/lead_mailer.rb
+++ app/mailers/lead_mailer.rb
@@ -1,6 +1,6 @@
 class LeadMailer < ApplicationMailer
 	def new_lead(lead)
 		@lead = lead
-		mail(to: "nate@modyo.com", from: @lead.email, subject: "You've received a message in nallenanderson.com")
+		mail(to: "nallenanderson@gmail.com", from: @lead.email, subject: "Recibiste un nuevo mensaje en Gourmet Fabregat y Co.")
 	end
 end
