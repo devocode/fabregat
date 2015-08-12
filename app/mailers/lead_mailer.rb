3e477400f6825fa5571c5628562049ffda06ddb2
Nathan Anderson <nallenanderson@gmail.com>
2015-08-12 17:26:45 -0300

    Changed mailer link

--- app/mailers/lead_mailer.rb
+++ app/mailers/lead_mailer.rb
@@ -1,6 +1,6 @@
 class LeadMailer < ApplicationMailer
 	def new_lead(lead)
 		@lead = lead
-		mail(to: "nallenanderson@gmail.com", from: @lead.email, subject: "Recibiste un nuevo mensaje en Gourmet Fabregat y Co.")
+		mail(to: "nallenanderson@gmail.com", from: @lead.email, subject: "Recibiste un nuevo mensaje | Gourmet Fabregat y Co.")
 	end
 end
