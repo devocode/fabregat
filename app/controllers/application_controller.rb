8f80bec9708dd1d35a4b1b6082ace284f3b8a26c
Nathan Anderson <nallenanderson@gmail.com>
2015-08-19 12:09:47 -0300

    Redirect Sign In to Leads

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,13 @@
 class ApplicationController < ActionController::Base
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
   protected
+
   def authenticate_user!
     redirect_to root_path, notice: "No tienes permiso para ver esta página." unless user_signed_in?
   end
+
+  def after_sign_in_path_for(resource)
+	  leads_path
+	end
 end
